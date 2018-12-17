498730507a1e038a8ea7edc2c5a29edb82ca49e3
Ana Cecília <cecivieira@gmail.com>
2018-12-18 00:23:04 +0100

    Solution

--- String Calculator Kata/StringCalculatorKata.rb
+++ String Calculator Kata/StringCalculatorKata.rb
@@ -1,11 +1,29 @@
 class StringCalculatorKata
     
-    def Add(*numbers)
-        print "Digite vários números: "
-        numbers = gets.chomp
-        print numbers
+    def Add(string)
+        sum = 0
+        array = string.split("\n")
+        if array.length == 1
+            string.each_char {|number|
+                sum += process_number(number)
+            }
+        elsif array.length == 0
+            return 0
+        else              
+            array_of_numbers = array[1].split   (array[0])
+            array_of_numbers.each {|number|
+                sum += process_number(number)
+            }
+        end
+        return sum
+    end
+
+    def process_number(number)
+        if number.to_i >= 0
+            return number.to_i
+        else 
+            raise "Negatives not allowed (#{number})"
+        end
     end
     
 end
-
-StringCalculatorKata.Add
--- String Calculator Kata/spec/TestStringCalculatorKata_spec.rb
+++ String Calculator Kata/spec/TestStringCalculatorKata_spec.rb
@@ -4,31 +4,50 @@ require_relative '../StringCalculatorKata'
 RSpec.describe StringCalculatorKata do
 
     describe "Add" do
-        it "Create a simple String calculator with a method int Add(string numbers). The method can take 
-        0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0)" do
-            stringcalculator = StringCalculatorKata.new
-            sum = stringcalculator.Add(2, 3)
-            expect(sum).to eq(5)        
-        end
-
-        it "Create a simple String calculator with a method int Add(string numbers). The method can take 
-        0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0)" do
-            stringcalculator = StringCalculatorKata.new
-            sum = stringcalculator.Add(2)
-            expect(sum).to eq(2)
-        end
-
-        it "Create a simple String calculator with a method int Add(string numbers). The method can take 
-        0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0)" do
-            stringcalculator = StringCalculatorKata.new
-            sum = stringcalculator.Add()
+        it "Should return zero when no arguments" do
+            stringCalculatorKata = StringCalculatorKata.new
+            sum = stringCalculatorKata.Add("")
             expect(sum).to eq(0)
         end
 
-        it "Allow the Add method to handle an unknown amount of numbers" do
-            stringcalculator = StringCalculatorKata.new
-            sum = stringcalculator.Add()
-            expect(sum).to eq()
+        it "Should return the number when only one parameter is passed" do
+            stringCalculatorKata = StringCalculatorKata.new
+            sum = stringCalculatorKata.Add("1")
+            expect(sum).to eq(1)
         end
+
+        it "Should return the sum of two numbers when two numbers are passed" do
+            stringCalculatorKata = StringCalculatorKata.new
+            sum = stringCalculatorKata.Add("8,1")
+            expect(sum).to eq(9)
+        end
+
+        it "Allow the Add method to handle an unknown amount of numbers" do
+            stringCalculatorKata = StringCalculatorKata.new
+            sum = stringCalculatorKata.Add("1,2,3,4,5")
+            expect(sum).to eq(15)
+        end
+
+        # it "Allow the Add method to handle new lines between numbers (instead of commas)" do
+        #     stringCalculatorKata = StringCalculatorKata.new
+        #     sum = stringCalculatorKata.Add("1\n2,3")
+        #     expect(sum).to eq(6)
+        # end
+        # Não é necessário escrever esse teste porque não há nova implementação no código. 
+        # O teste não falha, ou seja, não é necessário implementar nada para que o \n seja
+        # considerado como separador
+        
+        it "Support different delimiters, looks like this: [delimiter]\n[numbers...]" do
+            stringCalculatorKata = StringCalculatorKata.new
+            sum = stringCalculatorKata.Add("&\n4&2&1")
+            expect(sum).to eq(7)
+        end
+
+        it "Calling Add with a negative number will throw an exception \"negatives not allowed\" - and the negative that was passed." do
+            stringCalculatorKata = StringCalculatorKata.new            
+            expect{stringCalculatorKata.Add("&\n4&-2")}.to raise_error("Negatives not allowed (-2)")
+        end
+
+        
     end
 end
